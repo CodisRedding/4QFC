cbf5fb0464e79a4927c5cc4a3032646fcefba4a7
Rocky Assad <rocky.assad@premisehealth.com>
2022-03-18 21:26:58 -0500

    Add more teams

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TeamsComponent } from './teams/teams.component';
+// import { TeamsComponent } from './teams/teams.component';
 
 const routes: Routes = [
-  { path: 'teams', component: TeamsComponent },
-  { path: '', redirectTo: '/teams', pathMatch: 'full' }
+  // { path: 'teams', component: TeamsComponent },
+  // { path: '', redirectTo: '/teams', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
+import fantasyTeams from '../assets/week-3';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +9,21 @@ import { PrimeNGConfig } from 'primeng/api';
 })
 export class AppComponent implements OnInit {
   title = 'MLS Fantasy Team Generator';
+  teams = fantasyTeams;
+  positions: any[] = [];
 
-  constructor(private primengConfig: PrimeNGConfig) { }
+  constructor(private primengConfig: PrimeNGConfig) {
+    this.teams = this.teams.map((team, id) => Object.assign({ id }, team));
+  }
 
   ngOnInit() {
     this.primengConfig.ripple = true;
+
+    this.positions = [
+      { label: 'DEFENDER', value: 'DEFENDER' },
+      { label: 'MIDFIELDER', value: 'MIDFIELDER' },
+      { label: 'FORWARD', value: 'FORWARD' },
+      { label: 'KEEPER', value: 'KEEPER' }
+    ]
   }
 }
